2c8125dd83868feb4ce94d7daac84a8813609361
Laura Ladino Gallego <lladinog@unal.edu.co>
2025-12-02 15:13:52 -0500

    Refactor word counting functionality: reintroduce core functions and update main script for improved structure and clarity

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,40 +1,49 @@
-# Ejemplo para casos de uso
-# python3 -m homework data/input data/output
-import argparse
+import sys
 
-from homework.src._internals.count_words import count_words
-from homework.src._internals.preprocess_lines import preprocess_lines
-from homework.src._internals.read_all_lines import read_all_lines
-from homework.src._internals.split_into_words import split_into_words
-from homework.src._internals.write_word_counts import write_word_counts
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_into_words import split_into_words
+from ._internals.write_word_counts import write_word_counts
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Count Word in files.")
-    parser.add_argument(
-        "input", type=str, help="Path to the input folder containing files to process."
-    )
-    parser.add_argument(
-        "output",
-        type=str,
-        help="Path to the output folder containing files to process.",
-    )
+def parse_args(args=None):
+    if args is None:
+        args = sys.argv[1:]
 
-    parsed_args = parser.parse_args()
+    if len(args) != 2:
+        raise ValueError(
+            "Must provide exactly 2 arguments: input_folder and output_folder"
+        )
 
-    return parsed_args.input, parsed_args.output
+    input_folder, output_folder = args
+
+    # Asegurar slash final
+    if not input_folder.endswith("/"):
+        input_folder += "/"
+    if not output_folder.endswith("/"):
+        output_folder += "/"
+
+    return input_folder, output_folder
 
 
 def main():
     input_folder, output_folder = parse_args()
-    lines = read_all_lines(input_folder)
-    preprocessed_lines = preprocess_lines(lines)
-    words = split_into_words(preprocessed_lines)
-    word_counts = count_words(words)
-    write_word_counts(word_counts, output_folder)
+    ## read all lines
+    all_lines = read_all_lines(input_folder)
+
+    ### preprocess lines
+    all_lines = preprocess_lines(all_lines)
+
+    ### split in words
+    words = split_into_words(all_lines)
+
+    ### count words
+    counter = count_words(words)
+
+    ### write word counts
+    write_word_counts(counter, output_folder)
 
 
-# if __name__ == "__main__":
-#     parse_args()
-
-# input --data/input
+if __name__ == "__main__":
+    main()
